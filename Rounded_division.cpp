80c741da104d8357531078eabe4d545643416c9b
Rakibul Hasan <rakibulhasan105105@gmail.com>
2026-07-23 17:36:47 +0600

    here we go...

---
+++ Rounded_division.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define fastio  cin.tie(0); ios_base ::sync_with_stdio(0);
+#define ll long long 
+
+void phobia()
+{
+    double a,b;
+    cin >> a >> b;
+    //cout << fixed << setprecision(2) << a/b;
+    printf("%.2f\n", a/b);
+    
+ return;
+}
+
+int32_t main()
+{
+    fastio;
+    int tc = 1;
+   // cin >> tc;
+    while (tc--)
+    {
+        phobia();
+    }
+
+    return 0; //         Hey, it's like a phobia..
+}
